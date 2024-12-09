afdd49945af1f713774c8beb28ec16cb4f05d025
sungmin <room0729@gmail.com>
2024-12-09 16:13:40 +0900

    ..

---
+++ .script/docker_run.sh
@@ -0,0 +1,15 @@
+docker_image_name="cuda_12.1:v1.0"
+docker_container_name="triton"
+WORKSPACE=$(pwd)
+# Docker 실행
+docker run -it \
+  --name ${docker_container_name} \
+  -v ${HOME}:${HOME} \
+  -v ${WORKSPACE}:${WORKSPACE} \
+  -w ${WORKSPACE} \
+  --gpus all \
+  --shm-size=128g \
+  --ipc=host \
+  --hostname ${docker_container_name} \
+  ${docker_image_name}
+
